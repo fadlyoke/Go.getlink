742a5f312d1b9bf8677d88c9886c226a806cb80a
fadlyoke <71249492+fadlyoke@users.noreply.github.com>
2020-09-26 01:19:15 +0700

    Add files via upload

--- safelink_app.getlink.js
+++ safelink_app.getlink.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="post-1.html"}
 if(x=="2"){xxx="post-2.html"}
 if(x=="3"){xxx="post-3.html"}
-if(x=="4"){xxx="post-4.html"}a_to_ve[i].href="http://app.getlink.my.id/2020/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="post-4.html"}a_to_ve[i].href="http://web.getlink.my.id/2020/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
